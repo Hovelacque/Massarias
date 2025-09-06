67963d63618c8b37cd70f61d3dbf425f9f6bc10c
Gustavo <gustavo@kapibara.com.br>
2025-09-06 18:45:39 -0300

    Adc Angular Material

--- massarias/src/app/app.component.ts
+++ massarias/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatCardModule
+  ],
   templateUrl: 'app.component.html',
   styles: [],
 })
